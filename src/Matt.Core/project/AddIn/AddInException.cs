24842253bd901d256ed9c036a0327c3ef4af4ea1
imstilljustmatt <imstilljustmatt@gmail.com>
2025-07-20 12:36:09 +0800

    add XML docs to AddInException

--- src/Matt.Core/project/AddIn/AddInException.cs
+++ src/Matt.Core/project/AddIn/AddInException.cs
@@ -3,13 +3,35 @@
 namespace Matt.AddIn;
 
 
+/// <summary>
+/// Represents an error inside of the add-in.
+/// </summary>
 [Serializable]
 public class AddInException : Exception
 {
+	/// <summary>
+	/// Throws a new <seealso cref="AddInException"/>.
+	/// </summary>
 	public AddInException() { }
-	public AddInException(string message) : base(message) { }
-	public AddInException(string message, Exception inner) : base(message, inner) { }
 
+    /// <summary>
+    /// Throws a new <seealso cref="AddInException"/>.
+    /// </summary>
+	/// <param name="message">The value of the error message.</param>
+    public AddInException(string message) : base(message) { }
+
+    /// <summary>
+    /// Throws a new <seealso cref="AddInException"/>.
+    /// </summary>
+    /// <param name="message">The value of the error message.</param>
+    /// <param name="inner">The value of the existing error that may be related to this one.</param>
+    public AddInException(string message, Exception inner) : base(message, inner) { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
 	[Obsolete]
 	protected AddInException(
 	  System.Runtime.Serialization.SerializationInfo info,
