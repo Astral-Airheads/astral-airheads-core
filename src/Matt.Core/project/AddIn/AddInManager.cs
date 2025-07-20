a6c2dcc2998c67bce973f19f4bac956756fbf45b
imstilljustmatt <imstilljustmatt@gmail.com>
2025-07-20 12:31:17 +0800

    add XML docs to the AddInManager class

--- src/Matt.Core/project/AddIn/AddInManager.cs
+++ src/Matt.Core/project/AddIn/AddInManager.cs
@@ -6,11 +6,19 @@ using Matt.Validation;
 
 namespace Matt.AddIn;
 
+/// <summary>
+/// Just a simple add-in manager. :D
+/// </summary>
 public class AddInManager : IDisposable
 {
     private bool _disposed = false;
     private readonly List<IAddInBase> _addins = [];
 
+    /// <summary>
+    /// Gets an loaded add-in by its name. (Not case-sensitive)
+    /// </summary>
+    /// <param name="name">The value of the add-in name to find.</param>
+    /// <returns>The found add-in.</returns>
     public IAddInBase? GetAddInFromName(string name)
     {
         foreach (var addin in _addins)
@@ -23,6 +31,11 @@ public class AddInManager : IDisposable
         return null;
     }
 
+    /// <summary>
+    /// Gets an loaded add-in by its prefix. (Not case-sensitive)
+    /// </summary>
+    /// <param name="prefix">The value of the add-in prefix to find.</param>
+    /// <returns>The found add-in.</returns>
     public IAddInBase? GetAddInFromPrefix(string prefix)
     {
         foreach (var addin in _addins)
@@ -34,7 +47,11 @@ public class AddInManager : IDisposable
         }
         return null;
     }
-
+    
+    /// <summary>
+    /// Loads an add-in from a class library (*.dll) file.
+    /// </summary>
+    /// <param name="dllPath">The value of the DLL path.</param>
     public void LoadAddInFromFile(string dllPath)
     {
         Requires.FileExists(dllPath);
@@ -51,6 +68,12 @@ public class AddInManager : IDisposable
         LoadAddIn(addin);
     }
 
+    /// <summary>
+    /// Loads an add-in from an existing <seealso cref="IAddInBase"/>, whether
+    /// from an DLL or whatever.
+    /// </summary>
+    /// <param name="addin">The value of the add-in class.</param>
+    /// <exception cref="AddInException">Thrown when an add-in failed to initialize.</exception>
     public void LoadAddIn(IAddInBase addin)
     {
         Requires.NotNull(addin);
@@ -61,6 +84,10 @@ public class AddInManager : IDisposable
         _addins.Add(addin);
     }
 
+    /// <summary>
+    /// Unloads an add-in if it's found from the loaded add-ins list.
+    /// </summary>
+    /// <param name="addin">The value of the add-in class.</param>
     public void UnloadAddIn(IAddInBase addin)
     {
         Requires.NotNull(addin);
@@ -72,6 +99,10 @@ public class AddInManager : IDisposable
         _addins.Remove(addin);
     }
 
+    /// <summary>
+    /// Performs a task that disposes every resources used by the loaded add-ins and this manager.
+    /// </summary>
+    /// <param name="disposing">The value of whether to actually dispose them or nah.</param>
     protected virtual void Dispose(bool disposing)
     {
         if (!_disposed)
@@ -85,6 +116,9 @@ public class AddInManager : IDisposable
         }
     }
 
+    /// <summary>
+    /// Performs a task that disposes every resources used by the loaded add-ins and this manager.
+    /// </summary>
     public void Dispose()
     {
         Dispose(true);
