4ea193141122e03be7d37b3585409cd7e64b5180
imstilljustmatt <imstilljustmatt@gmail.com>
2025-07-22 13:32:31 +0800

    added my stupidity there

---
+++ src/Matt.Core/project/Stupidity/NotAConsoleUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Matt.Stupidity;
+
+/// <summary>
+/// A random Console utility, for fun. :D
+/// </summary>
+public sealed class NotAConsoleUtil
+{
+    public static void WriteLine(int delay, string text)
+    {
+        foreach (var i in text)
+        {
+            Console.Write(i);
+            Thread.Sleep(delay);
+        }
+    }
+
+
+    public static void WriteLine(int delay, string format, params object?[] args)
+    {
+        foreach (var i in string.Format(format, args))
+        {
+            Console.Write(i);
+            Thread.Sleep(delay);
+        }
+    }
+}
