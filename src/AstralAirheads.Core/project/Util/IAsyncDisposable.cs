85e346e0346c3f701e3d42d2666f6e8970abf115
imstilljustmatt <imstilljustmatt@gmail.com>
2025-07-27 13:46:45 +0800

    add IAsyncDisposable

---
+++ src/AstralAirheads.Core/project/Util/IAsyncDisposable.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace AstralAirheads.Util;
+
+/// <summary>
+/// Provides a mechanism for releasing unmanaged resources asynchronously.
+/// </summary>
+public interface IAsyncDisposable
+{
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or
+    /// resetting unmanaged resources asynchronously.
+    /// </summary>
+    Task DisposeAsync();
+}
