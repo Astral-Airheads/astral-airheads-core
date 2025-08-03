7a325dfe05a70fe85c95e76ae2652c062c3ae777
imstilljustmatt <imstilljustmatt@gmail.com>
2025-08-03 17:05:13 +0800

    update release notes

--- src/AstralAirheads.Core/project/Stupidity/NotAConsoleUtil.cs
+++ src/AstralAirheads.Core/project/Stupidity/NotAConsoleUtil.cs
@@ -21,7 +21,7 @@ public sealed class NotAConsoleUtil
     /// </summary>
     /// <param name="delay"></param>
     /// <param name="text"></param>
-    public static void WriteLine(int delay, string text)
+    public static void Write(int delay, string text)
     {
         foreach (var i in text)
         {
@@ -36,9 +36,37 @@ public sealed class NotAConsoleUtil
     /// <param name="delay"></param>
     /// <param name="format"></param>
     /// <param name="args"></param>
-    public static void WriteLine(int delay, string format, params object?[] args)
+    public static void Write(int delay, string format, params object?[] args)
     {
         foreach (var i in string.Format(format, args))
+        {
+            Console.Write(i);
+            Thread.Sleep(delay);
+        }
+    }
+    /// <summary>
+    /// This is utterly fucking retarded LMAO.
+    /// </summary>
+    /// <param name="delay"></param>
+    /// <param name="text"></param>
+    public static void WriteLine(int delay, string text)
+    {
+        foreach (var i in text + "\n")
+        {
+            Console.Write(i);
+            Thread.Sleep(delay);
+        }
+    }
+
+    /// <summary>
+    /// This is utterly fucking retarded LMAO.
+    /// </summary>
+    /// <param name="delay"></param>
+    /// <param name="format"></param>
+    /// <param name="args"></param>
+    public static void WriteLine(int delay, string format, params object?[] args)
+    {
+        foreach (var i in string.Format(format + "\n", args))
         {
             Console.Write(i);
             Thread.Sleep(delay);
